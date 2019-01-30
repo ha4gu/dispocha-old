121123f640dd1401893d2e05eace1a8b9df81974
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-30 10:33:03 +0900

    edit migration file and rails db:migrate

--- db/migrate/20190130012544_create_rooms.rb
+++ db/migrate/20190130012544_create_rooms.rb
@@ -2,9 +2,7 @@ class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
       t.string :code
-      t.integer :owner_id
       t.string :name
-      t.datetime :last_post_at
 
       t.timestamps
     end
