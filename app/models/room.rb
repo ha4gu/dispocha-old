b960bf2941faae0e3808997b9c5d804dffd63b12
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-01-30 12:57:05 +0900

    add validation for Room.code and its test

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,11 @@
 class Room < ApplicationRecord
+
+  # validation
+  VALID_CODE_REGEX = /\A[\w-]+\z/ # a-z A-Z 0-9 _ -
+  validates :code,
+    presence: true,
+    uniqueness: true,
+    length: { in: 8..255 },
+    format: { with: VALID_CODE_REGEX}
+
 end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,69 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @room = Room.new(
+      code: "Vk3e-6xudmo_FSi3jL",
+      name: "Example Room",
+    )
+    @MIN_CODE_LENGTH = 8
+    @MAX_CODE_LENGTH = 255
+  end
+
+  test "should be valid" do
+    assert @room.valid?
+  end
+
+  test "code should be present" do
+    @room.code = "    "
+    assert_not @room.valid?
+  end
+
+  test "code should be unique" do
+    @room.code = "MyString1" # already in fixtures
+    assert_not @room.valid?
+  end
+
+  test "code should be in appropriate length" do
+    @room.code = "a"*(@MIN_CODE_LENGTH - 1)
+    assert_not @room.valid?
+    @room.code = "a"*@MIN_CODE_LENGTH
+    assert @room.valid?
+    @room.code = "a"*@MAX_CODE_LENGTH
+    assert @room.valid?
+    @room.code = "a"*(@MAX_CODE_LENGTH + 1)
+    assert_not @room.valid?
+  end
+
+  test "code should use only alphabet, numer, _, and -" do
+    assert @room.valid?
+    @room.code = "AbCdEfGhIjK"
+    assert @room.valid?
+    @room.code = "0123456789"
+    assert @room.valid?
+    @room.code = "----------"
+    assert @room.valid?
+    @room.code = "__________"
+    assert @room.valid?
+    @room.code = "A-Cd_f h1jK" # can't use space
+    assert_not @room.valid?
+    @room.code = "A-Cd_f@h1jK" # can't use @
+    assert_not @room.valid?
+    @room.code = "A-Cd_f=h1jK" # can't use =
+    assert_not @room.valid?
+    @room.code = "A-Cd_f#h1jK" # can't use #
+    assert_not @room.valid?
+    @room.code = "A-Cd_f/h1jK" # can't use /
+    assert_not @room.valid?
+    @room.code = "A-Cd_f?h1jK" # can't use ?
+    assert_not @room.valid?
+    @room.code = "A-Cd_f+h1jK" # can't use +
+    assert_not @room.valid?
+    @room.code = "A-Cd_f!h1jK" # can't use !
+    assert_not @room.valid?
+    @room.code = "A-Cd_fあh1jK" # can't use multibytes
+    assert_not @room.valid?
+  end
+
 end
