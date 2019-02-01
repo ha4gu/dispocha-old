dca3498d1a8a1ff8b1ee508c7476e1fe9d9cbbd4
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-02-01 10:23:10 +0900

    add some tests for room

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -4,13 +4,56 @@ class RoomsControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @base_title = "すてチャ(仮)"
+    @room1 = rooms(:one)
   end
 
-  # /new
+
+  # new
   test "should get new" do
     get new_room_path
     assert_response :success
+    assert_template 'rooms/new'
     assert_select "title", "ルーム作成 - #{@base_title}"
+    assert_not assigns(:room).code.nil?
+  end
+
+
+  # create
+
+  test "should fail to save room" do
+    # validationに引っかかってルーム作成に失敗するパターン
+    assert_no_difference 'Room.count' do
+      post create_room_path, params: { room: { code: "    ", name: "    " } }
+    end
+    assert_not flash.empty?
+    assert_template 'rooms/new'
+  end
+
+  test "should succeed to save room" do
+    # validationをクリアしてルーム作成に成功するパターン
+    assert_difference 'Room.count', 1 do
+      post create_room_path, params: { room: {
+        code: "nWE910-amElAz_5", name: "👆Yet Another Test Room, Yeah!" } }
+    end
+    assert_not flash.empty?
+    assert_redirected_to room_path(assigns(:room).code)
+  end
+
+
+  # show
+
+  test "should be redirected when accessed non-exist room" do
+    # 存在しないルームにアクセスしてリダイレクトされるパターン
+    get room_path("non-exist_room")
+    assert_not flash.empty?
+    assert_redirected_to root_path
+  end
+
+  test "should be able to access room page" do
+    # 存在しているルームにアクセスするパターン
+    get room_path(@room1.code)
+    assert_response :success
+    assert_template 'rooms/show'
   end
 
 end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -22,7 +22,7 @@ class RoomTest < ActiveSupport::TestCase
   end
 
   test "code should be unique" do
-    @room.code = "MyString1" # already in fixtures
+    @room.code = "MyTestRoom1" # already in fixtures
     assert_not @room.valid?
   end
 
@@ -77,6 +77,25 @@ class RoomTest < ActiveSupport::TestCase
     assert @room.valid?
     @room.name = "a"*(@MAX_NAME_LENGTH + 1)
     assert_not @room.valid?
+  end
+
+  test "test for self.random_code" do
+    code = Room.random_code
+    assert_kind_of String, code # 文字列である
+    assert_equal 8, code.length # 8文字である
+    assert code.delete("a-zA-Z0-9_-").blank? # alphanum_-以外が使われていない
+    code2 = Room.random_code
+    assert_not_equal code, code2 # ほぼ毎回異なる文字列を生成する
+  end
+
+  test "test for .has_valid_code?" do
+    assert @room.has_valid_code? # codeが有効であることを確認
+    assert @room.errors.messages.count.zero? # エラーを持ち越さない
+    @room.code = "P@ssw0rd" # 無効なcode
+    assert_not @room.valid? # @roomが無効であることを確認
+    assert_not @room.errors.messages.count.zero? # エラーがあることを確認
+    assert_not @room.has_valid_code? # codeが無効であることを確認
+    assert @room.errors.messages.count.zero? # エラーを持ち越さない
   end
 
 end
