83a81d4d24df90adef73e01ec72066cf90d44d3f
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-02-25 16:20:43 +0900

    mod top page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def top
-    @rooms = Room.all
+    # 開発環境の場合のみ、デバッグ表示のためにルーム一覧を取得しておく
+    if Rails.env.development?
+      @rooms = Room.all
+    end
   end
 
 end
