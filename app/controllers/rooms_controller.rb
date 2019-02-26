1fb6c9e13b3a60c289f44686ffabd56b48faea31
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-02-26 17:59:27 +0900

    various changes

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,11 @@
 class RoomsController < ApplicationController
-  before_action :check_signed_in, only: [:show]
+  before_action :check_signed_in, only: [:show, :user_update]
+  before_action :check_correct_room, only: [:show, :user_update]
 
   def new
     @room = Room.new
-    # 初期値の設定
-    if @room.code.nil? # 保存失敗時に上書きしないように
-      @room.code = Room.random_code
+    # ルームID（.code）の初期値を自動生成する
+    if @room.code.nil?
       until @room.has_valid_code?
         @room.code = Room.random_code
       end
@@ -13,41 +13,70 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new(user_params)
-    if @room.save
-      # 保存に成功した場合の処理
+    @room = Room.new(room_params)
+    if @room.save # 保存に成功した場合
       flash[:success] = "ルームが作成されました。"
-      redirect_to room_path(@room.code)
-    else
-      # 保存に失敗した場合の処理
+      flash[:info] = "ユーザ登録を行ってください。"
+      redirect_to new_user_registration_path(@room.code) # ユーザ新規登録へ移動
+    else # 保存に失敗した場合
       flash.now[:danger] = "ルームの作成に失敗しました。"
       render 'new'
     end
   end
 
   def show
-    # ルームが見つからない場合
+    @post = Post.new
+    @room = Room.find_by(code: params[:room_id])
+    # ルームが見つからない場合にはトップページへ移動
     if @room.nil?
       flash[:warning] = "ルーム #{params[:room_id]} が見つかりません。"
       redirect_to root_path
     end
-    @post = Post.new
   end
 
-  def check_signed_in
-    @room = Room.find_by(code: params[:room_id])
-    unless user_signed_in?
-      flash[:danger] = "ログインしてください。"
-      redirect_to new_user_session_path(@room.code)
-    end
+  def user_edit
+    @user = User.find(current_user.id)
   end
+
+  def user_update
+    @user = User.find(current_user.id)
+    @room = Room.find_by(code: params[:room_id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to room_path(params[:room_id])
+    else
+      flash.now[:danger] = "Failed to update user info"
+      render 'devise/registrations/edit'
+    end
+
+  end
+
 
   private
 
-  def user_params
+  def room_params
     params.require(:room).permit(:code, :name)
   end
 
+  def user_params
+    params.require(:user).permit(:name, :display_name, :icon)
+  end
 
+  # before_actions
 
+  def check_signed_in
+    room = Room.find_by(code: params[:room_id])
+    unless user_signed_in?
+      flash[:danger] = "ログインしてください。"
+      redirect_to new_user_session_path(room.code)
+    end
+  end
+
+  def check_correct_room
+    unless current_user.room.code == params[:room_id]
+      flash[:danger] = "ログイン中のアカウントではこのルームにアクセスできません。
+                        一度ログアウトしてからログインし直してください。"
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
 
   # Rooms
   get  '/new',      to: 'rooms#new',    as: 'new_room'
-  post '/create',   to: 'rooms#create', as: 'create_room'
+  post '/new',      to: 'rooms#create', as: 'create_room'
   get  '/:room_id', to: 'rooms#show',   as: 'room'
-  #post '/:room_id/room_update', to: 'rooms#room_update', as: 'user_update'
-  post '/:room_id/user_update', to: 'rooms#user_update', as: 'user_update'
+  # get   '/:room_id/room_edit', to: 'rooms#room_edit',   as: 'room_edit'   # あとで作る
+  # patch '/:room_id/room_edit', to: 'rooms#room_update', as: 'room_update' # あとで作る
+  get   '/:room_id/user_edit', to: 'rooms#user_edit',   as: 'user_edit'
+  patch '/:room_id/user_edit', to: 'rooms#user_update', as: 'user_update'
 
   # devise
   devise_for :users, path: ':room_id', path_names: {
